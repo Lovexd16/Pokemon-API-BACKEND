2cbe5c88edbe553146c839c3fde2f8b370b04c64
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-01-29 13:56:12 +0100

    början av mapp och fil struktur

--- pokemonapi/src/main/java/com/pokemonapi/pokemonapi/api/controller/PokemonController.java
+++ pokemonapi/src/main/java/com/pokemonapi/pokemonapi/api/controller/PokemonController.java
@@ -1,25 +1,9 @@
 package com.pokemonapi.pokemonapi.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.pokemonapi.pokemonapi.api.model.Pokemon;
-import com.pokemonapi.pokemonapi.service.PokemonService;
 
 @RestController
 public class PokemonController {
 
-    private PokemonService pokemonService;
-
-    @Autowired
-    public PokemonController(PokemonService pokemonService) {
-        this.pokemonService = pokemonService;
-    }
-    
-    @GetMapping("/pokemon")
-    public Pokemon getPokemon(@RequestParam Integer id) {
-        return pokemonService.getPokemon(id);
-    }
 }
+
--- pokemonapi/src/main/java/com/pokemonapi/pokemonapi/service/PokemonService.java
+++ pokemonapi/src/main/java/com/pokemonapi/pokemonapi/service/PokemonService.java
@@ -1,31 +1,8 @@
 package com.pokemonapi.pokemonapi.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import com.pokemonapi.pokemonapi.api.model.Pokemon;
 
 @Service
 public class PokemonService {
-
-    //Hämta från databas istället för hårdkodad lista
-    private List<Pokemon> pokemonList;
-
-    public PokemonService() {
-        pokemonList = new ArrayList<>();
-        pokemonList.add(new Pokemon(1, "Charmander", "Fire"));
-        pokemonList.add(new Pokemon(2, "Squirtle", "Water"));
-        pokemonList.add(new Pokemon(3, "Bulbasaur", "Grass"));
-    }
-    
-    //Metod för att hämta individuell pokemon
-    public Pokemon getPokemon(Integer id) {
-        return pokemonList.stream()
-        .filter(pokemon -> id.equals(pokemon.getId()))
-        .findFirst()
-        .orElse(null);
-    }
 
 }
